5b0dec8848926ca906fd57ee04116f27d48489c8
RobsonVinicius <robsondgn@outlook.com>
2017-02-08 23:40:36 -0200

    site

---
+++ js/land.js
@@ -0,0 +1,45 @@
+function atualizeNavbar(){
+  var scrollTopNun = $(window).scrollTop();
+  if(scrollTopNun > 10){
+    $("#menu").addClass("bg-roxo");
+  }else{
+    $("#menu").removeClass("bg-roxo");
+  }
+}
+
+$(document).ready(function(){
+  atualizeNavbar();
+  
+  $(window).scroll(function(){
+    atualizeNavbar();
+  })
+  
+  $(".page-scroll").click(function(e){
+        e.preventDefault();
+        var hash = this.hash;
+
+        $("html, body").animate({
+            scrollTop: $(hash).offset().top-73
+        }, 500);   
+    
+    })
+  
+  $(".videoModal").click(function(e){
+        e.preventDefault();
+
+        var id_vimeo = $(this).data("video");
+
+        var link ="https://player.vimeo.com/video/"+id_vimeo+"?autoplay=1&title=0&byline=0&portrait=0";
+
+        var titulo = $(this).data("titulo");
+
+        $("#videoModal .modal-title").html(titulo);
+        $(".video-modal").attr("src", link);
+
+        $("#videoModal").modal('show');
+    })
+    
+    $("#videoModal").on("hidden.bs.modal", function(e){
+        $(".video-modal").attr("src", "https://player.vimeo.com/video/180935055");
+    })
+})
